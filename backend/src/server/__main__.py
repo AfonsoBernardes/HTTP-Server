edb9c3531a37bec144b4a422b673efdfb9d7547c
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2025-12-11 17:52:03 +0100

    Allow server (socket) to reuse address for connections
    Try threading the acceptance of connection to the server.

--- backend/src/server/__main__.py
+++ backend/src/server/__main__.py
@@ -6,11 +6,7 @@ def run_server():
     # Find out what the structure of a request is like and start parsing line by line.
     # Depending on the request, return a proper response; let's do a "200 OK" or a "400 Bad Request" for now.
     server = HttpServer()
-    while True:
-        client_connection, client_address = server.run_server()
-        print(f"Client Conn: {client_connection}, Client Address: {client_address}")
-
-        request = client_connection.recv(1024).decode('utf-8')
+    server.run_server()
 
 
 if __name__ == "__main__":
--- backend/src/server/core.py
+++ backend/src/server/core.py
@@ -1,32 +1,50 @@
 import os
-from socket import AF_INET, SOCK_STREAM, socket
+import threading
+from socket import AF_INET, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
 
 
 class HttpServer:
     def __init__(self):
         # AF_INET is the Internet address family for IPv4.
         # SOCK_STREAM is the socket type for TCP, the protocol that will be used to transport messages in the network.
-        self.socket = socket(family=AF_INET, type=SOCK_STREAM)
+        self.server_socket = socket(family=AF_INET, type=SOCK_STREAM)
+
+        # SOL_SOCKET -> there are socket-level options, IP-level options, TCP-level options, SOL_SOCKET makes option affect the socket object itself as a whole.
+        # REUSEADDR -> socket-level option which allows the server to reuse (i.e., accept connections) the same ip and port, while it's in close-wait or time-wait state.
+        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
         self.host = os.getenv("HOST", "0.0.0.0")
         self.port = int(os.getenv("BACKEND_PORT", "8080"))
 
-    def run_server(self) -> tuple[socket, int]:
+    def run_server(self) -> tuple[socket, tuple[str, int]]:
         # associate the socket with the server address
-        self.socket.bind((self.host, self.port))
+        self.server_socket.bind((self.host, self.port))
 
         # puts the socket into server mode, listening for up to "n" connections
-        self.socket.listen(1)
+        self.server_socket.listen(1)
 
-        # waits for an incoming connection
-        # conn is a new socket object usable to send and receive data on the connection,
-        # address is the address bound to the socket on the other end of the connection.
-        client_connection, client_address = self.socket.accept()
+        while True:
+            # waits for an incoming connection
+            # conn is a new socket object usable to send and receive data on the connection,
+            # address is the address bound to the socket on the other end of the connection.
+            client_connection, client_address = self.server_socket.accept()
 
-        return client_connection, client_address
+            thread = threading.Thread(target=self.handle_request, args=(client_connection,))
+            thread.start()
+
+    @staticmethod
+    def handle_request(client_connection: socket):
+        client_connection.recv(1024).decode("utf-8")
+
+        response = "Hello World!"
+
+        # 📤 Send response
+        client_connection.send(response.encode("utf-8"))
+        client_connection.close()
 
     def __enter__(self):
         self.run_server()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(f"Stopping server on {self.host}:{self.port}")
-        self.socket.close()
+        self.server_socket.close()
