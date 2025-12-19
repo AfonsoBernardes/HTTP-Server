120c4367497d362ea6f408501ebb0d59ae0b3ef7
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2025-12-19 10:39:59 +0100

    Return mock response from http server when handling request

--- backend/src/server/http_server.py
+++ backend/src/server/http_server.py
@@ -4,6 +4,16 @@ from server.tcp_server import TCPServer
 
 
 class HTTPServer(TCPServer):
-    @override
-    def handle_error(self, request, client_address):
-        pass
+    def handle_request(self, data) -> str:
+        status_line = "HTTP/1.1 200 OK\r\n"
+        response_body = """
+        <html>
+            <body>
+                <h1>Request received!</h1>
+            <body>
+        </html>
+        """
+
+        response = f"{status_line} {response_body}"
+
+        return response
