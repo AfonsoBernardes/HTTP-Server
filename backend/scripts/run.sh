bc6bae83b60186a0ea87f8714b5c31b3a57a68c3
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-01-02 17:26:39 +0100

    Remove comment

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -2,5 +2,4 @@
 
 VENV="${UV_PROJECT_ENVIRONMENT:-.venv}"
 
-# TODO: should this be a package known in the entire codebase? If so, how does setuptools help us here?
 "$VENV/bin/python3" -BbbOOum server
