2477f38bd8c4f69c43e49b99e30dc181b9202cba
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-01-03 16:13:10 +0100

    Add exceptions for invalid method and protocol

--- backend/src/requests/exceptions.py
+++ backend/src/requests/exceptions.py
@@ -1,6 +1,21 @@
 from displayable_exceptions.displayable_exception import DisplayableException
+from requests.schema import RequestMethod, RequestProtocol
 
 
 class InvalidDecoding(DisplayableException):
     def __init__(self):
         super().__init__("unable to decode request")
+
+
+class InvalidHTTPMethod(DisplayableException):
+    EXPECTED_METHODS = [method.value for method in RequestMethod]
+
+    def __init__(self, method: str):
+        super().__init__(f"invalid HTTP method: expected {", ".join(self.EXPECTED_METHODS)}, got {method!r}")
+
+
+class InvalidHTTPProtocol(DisplayableException):
+    EXPECTED_PROTOCOLS = [protocol.value for protocol in RequestProtocol]
+
+    def __init__(self, protocol: str):
+        super().__init__(f"invalid HTTP protocol: expected {", ".join(self.EXPECTED_PROTOCOLS)}, got {protocol}")
