3cb8d8d9c36d0665b67411ea2aca4550216c99b1
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-01-05 20:34:53 +0100

    Write test for valid/ invalid RequestProtocol

--- backend/tests/unit/test_requests.py
+++ backend/tests/unit/test_requests.py
@@ -1,7 +1,7 @@
 import pytest
 from asserts import assert_equal, assert_raises
 
-from requests.exceptions import InvalidHTTPMethod, InvalidRequest
+from requests.exceptions import InvalidHTTPMethod, InvalidRequest, InvalidHTTPProtocol
 from requests.request import Request
 from requests.schema import RequestMethod, RequestProtocol
 
@@ -20,7 +20,7 @@ class TestRequestMethod:
         raw_data = f"{request_method} / HTTP/1.1\r\n\r\n".encode("utf-8")
         request = Request(raw_data)
 
-        assert_equal(request.method, request_method)
+        assert_equal(request.method, RequestMethod(request_method))
         assert_equal(request.target, "/")
         assert_equal(request.protocol, RequestProtocol.HTTP_1_1)
         assert_equal(request.headers, {})
@@ -43,14 +43,52 @@ class TestRequestMethod:
             Request(raw_data)
 
 
+
+class TestRequestProtocol:
+    EXPECTED_PROTOCOL = ", ".join(protocol.value for protocol in RequestProtocol)
+
     @pytest.mark.parametrize(
-        "invalid_byte_request",
+        "request_protocol",
         [
-            b"GET / HTTP/1.1",
-            b"GET / HTTP/1.1\r\n",
+            request_protocol.value for request_protocol in RequestProtocol
         ],
     )
     @pytest.mark.asyncio
-    async def test_should_fail_to_parse_request_without_two_linebreaks(self, invalid_byte_request):
-        with assert_raises(InvalidRequest):
-            Request(invalid_byte_request)
+    async def test_should_parse_request_with_valid_protocol(self, request_protocol: str):
+        raw_data = f"GET / {request_protocol}\r\n\r\n".encode("utf-8")
+        request = Request(raw_data)
+
+        assert_equal(request.method, "GET")
+        assert_equal(request.target, "/")
+        assert_equal(request.protocol, RequestProtocol(request_protocol))
+        assert_equal(request.headers, {})
+        assert_equal(request.body, None)
+
+
+    @pytest.mark.parametrize(
+        "invalid_request_protocol, error_message",
+        [
+            (None, f"invalid HTTP protocol: expected [{EXPECTED_PROTOCOL}], got 'None'"),
+            ("", f"invalid HTTP protocol: expected [{EXPECTED_PROTOCOL}], got '')"),
+            (" ", f"invalid HTTP protocol: expected [{EXPECTED_PROTOCOL}], got ' ')"),
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_should_fail_to_parse_request_with_invalid_protocol(self, invalid_request_protocol, error_message: str):
+        raw_data = f"GET / {invalid_request_protocol}\r\n\r\n".encode("utf-8")
+
+        with assert_raises(InvalidHTTPProtocol, error_message):
+            Request(raw_data)
+
+
+@pytest.mark.parametrize(
+    "invalid_byte_request",
+    [
+        b"GET / HTTP/1.1",
+        b"GET / HTTP/1.1\r\n",
+    ],
+)
+@pytest.mark.asyncio
+async def test_should_fail_to_parse_request_without_two_linebreaks(invalid_byte_request):
+    with assert_raises(InvalidRequest):
+        Request(invalid_byte_request)
