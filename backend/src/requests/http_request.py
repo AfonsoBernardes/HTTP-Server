f2bc147bec2cb89aa40b6d15c64d3a1cf34fcb55
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-03-14 20:53:17 +0100

    HTTP request does not handle raw data due to possibility of data coming in chunks

--- backend/src/requests/http_request.py
+++ backend/src/requests/http_request.py
@@ -1,11 +1,9 @@
-from typing import Optional
+from typing import Dict, Optional
 
 from requests.exceptions import (
-    InvalidDecoding,
     InvalidHTTPHeaders,
     InvalidHTTPMethod,
     InvalidHTTPProtocol,
-    InvalidRequest,
 )
 from requests.schema import HTTPRequestMethod, HTTPRequestProtocol
 
@@ -14,44 +12,38 @@ class HTTPRequest:
     method: HTTPRequestMethod
     target: str
     protocol: HTTPRequestProtocol
-    headers: dict[str, str]
-    body: Optional[bytes]
+    headers: Dict[str, str]
+    body: Optional[str]
 
-    def __init__(self, raw_data: bytes):
-        self.parse_request(raw_data)
+    def __init__(self):
+        self.headers: Dict[str, str] = {}
+        self.body = None
 
-    def parse_request(self, raw_data: bytes):
-        try:
-            request_headers, request_body = raw_data.split(b"\r\n\r\n", maxsplit=1)
-            request_headers = request_headers.decode("utf-8")  # decode only headers
-        except UnicodeDecodeError:
-            raise InvalidDecoding()
-        except ValueError:
-            raise InvalidRequest()
-        else:
-            self.body = request_body if request_body else None
-            request_headers = request_headers.splitlines()
+    def parse_headers(self, request_headers: str):
+        request_headers = request_headers.splitlines()
 
-        # parse start-line: <METHOD> <TARGET> <PROTOCOL>
-        start_line = request_headers.pop(0)
-        start_line = start_line.split(" ")
+        # parse request line: <METHOD> <TARGET> <PROTOCOL>
+        request_line = request_headers.pop(0)
+        request_line = request_line.split(" ")
 
-        try:
-            self.method = HTTPRequestMethod(start_line[0])
-        except ValueError:
-            raise InvalidHTTPMethod(start_line[0])
-
-        self.target = start_line[1]
-
-        try:
-            self.protocol = HTTPRequestProtocol(start_line[2])
-        except ValueError:
-            raise InvalidHTTPProtocol(start_line[2])
-
-        self.headers = {}
         for header in request_headers:
             try:
                 key, value = header.split(": ", maxsplit=1)
                 self.headers[key] = value
             except ValueError:
                 raise InvalidHTTPHeaders()
+
+        try:
+            self.method = HTTPRequestMethod(request_line[0])
+        except ValueError:
+            raise InvalidHTTPMethod(request_line[0])
+
+        self.target = request_line[1]
+
+        try:
+            self.protocol = HTTPRequestProtocol(request_line[2])
+        except ValueError:
+            raise InvalidHTTPProtocol(request_line[2])
+
+    def parse_body(self, request_body: str):
+        self.body = request_body if request_body else None
--- backend/tests/unit/test_http_request.py
+++ backend/tests/unit/test_http_request.py
@@ -1,55 +1,10 @@
 import pytest
 from asserts import assert_equal, assert_raises
 
-from requests.exceptions import InvalidHTTPMethod, InvalidRequest, InvalidHTTPProtocol, InvalidHTTPHeaders
+from requests.exceptions import InvalidHTTPMethod, InvalidHTTPProtocol, InvalidHTTPHeaders
 from requests.http_request import HTTPRequest
 from requests.schema import HTTPRequestMethod, HTTPRequestProtocol
 
-
-class TestRequestParsing:
-    @pytest.mark.parametrize(
-        "invalid_byte_request",
-        [
-            b"GET / HTTP/1.1",
-            b"GET / HTTP/1.1\r\n",
-        ],
-    )
-    @pytest.mark.asyncio
-    async def test_should_fail_to_parse_request_without_two_linebreaks(self, invalid_byte_request):
-        with assert_raises(InvalidRequest):
-            HTTPRequest(invalid_byte_request)
-
-    @pytest.mark.parametrize(
-        "request_headers, expected_headers",
-        [
-            ("", {}),
-            ("Server: Test Server", {"Server": "Test Server"}),
-            ("Server: Test Server\r\nContent Type: text/html", {"Server": "Test Server", "Content Type": "text/html"}),
-        ],
-    )
-    @pytest.mark.asyncio
-    async def test_should_parse_request_with_valid_headers(self, request_headers: str, expected_headers: dict):
-        raw_data = f'GET / HTTP/1.1\r\n{request_headers}\r\n\r\n'.encode("utf-8")
-        request = HTTPRequest(raw_data)
-
-        assert_equal(request.method, HTTPRequestMethod.GET)
-        assert_equal(request.target, "/")
-        assert_equal(request.protocol, HTTPRequestProtocol.HTTP_1_1)
-        assert_equal(request.headers, expected_headers)
-
-    @pytest.mark.parametrize(
-        "invalid_request_headers",
-        [
-            "Server- Test",
-            "Server Test Server",
-        ],
-    )
-    @pytest.mark.asyncio
-    async def test_should_fail_to_parse_request_with_invalid_headers(self, invalid_request_headers: str):
-        raw_data = f'GET / HTTP/1.1\r\n{invalid_request_headers}\r\n\r\n'.encode("utf-8")
-
-        with assert_raises(InvalidHTTPHeaders):
-            HTTPRequest(raw_data)
 
 class TestRequestMethod:
     EXPECTED_METHODS = ", ".join(method.value for method in HTTPRequestMethod)
@@ -62,8 +17,10 @@ class TestRequestMethod:
     )
     @pytest.mark.asyncio
     async def test_should_parse_request_with_valid_method(self, request_method: str):
-        raw_data = f"{request_method} / HTTP/1.1\r\n\r\n".encode("utf-8")
-        request = HTTPRequest(raw_data)
+        data = f"{request_method} / HTTP/1.1"
+
+        request = HTTPRequest()
+        request.parse_headers(data)
 
         assert_equal(request.method, HTTPRequestMethod(request_method))
         assert_equal(request.target, "/")
@@ -82,10 +39,11 @@ class TestRequestMethod:
     )
     @pytest.mark.asyncio
     async def test_should_fail_to_parse_request_with_invalid_method(self, invalid_request_method, error_message: str):
-        raw_data = f"{invalid_request_method} / HTTP/1.1\r\n\r\n".encode("utf-8")
+        data = f"{invalid_request_method} / HTTP/1.1"
 
+        request = HTTPRequest()
         with assert_raises(InvalidHTTPMethod, error_message):
-            HTTPRequest(raw_data)
+            request.parse_headers(data)
 
 
 
@@ -100,8 +58,10 @@ class TestRequestProtocol:
     )
     @pytest.mark.asyncio
     async def test_should_parse_request_with_valid_protocol(self, request_protocol: str):
-        raw_data = f"GET / {request_protocol}\r\n\r\n".encode("utf-8")
-        request = HTTPRequest(raw_data)
+        data = f"GET / {request_protocol}"
+
+        request = HTTPRequest()
+        request.parse_headers(data)
 
         assert_equal(request.method, "GET")
         assert_equal(request.target, "/")
@@ -120,7 +80,45 @@ class TestRequestProtocol:
     )
     @pytest.mark.asyncio
     async def test_should_fail_to_parse_request_with_invalid_protocol(self, invalid_request_protocol, error_message: str):
-        raw_data = f"GET / {invalid_request_protocol}\r\n\r\n".encode("utf-8")
+        data = f"GET / {invalid_request_protocol}"
 
+        request = HTTPRequest()
         with assert_raises(InvalidHTTPProtocol, error_message):
-            HTTPRequest(raw_data)
+            request.parse_headers(data)
+
+
+class TestRequestHeadersParsing:
+    @pytest.mark.parametrize(
+        "request_headers, expected_headers",
+        [
+            ("", {}),
+            ("Server: Test Server", {"Server": "Test Server"}),
+            ("Server: Test Server\r\nContent-Type: text/html", {"Server": "Test Server", "Content-Type": "text/html"}),
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_should_parse_valid_request_headers(self, request_headers: str, expected_headers: dict):
+        data = f'GET / HTTP/1.1\r\n{request_headers}'
+
+        request = HTTPRequest()
+        request.parse_headers(data)
+
+        assert_equal(request.method, HTTPRequestMethod.GET)
+        assert_equal(request.target, "/")
+        assert_equal(request.protocol, HTTPRequestProtocol.HTTP_1_1)
+        assert_equal(request.headers, expected_headers)
+
+    @pytest.mark.parametrize(
+        "invalid_request_headers",
+        [
+            "Server- Test",
+            "Server Test Server",
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_should_fail_to_parse_invalid_request_headers(self, invalid_request_headers: str):
+        data = f'GET / HTTP/1.1\r\n{invalid_request_headers}'
+
+        request = HTTPRequest()
+        with assert_raises(InvalidHTTPHeaders):
+            request.parse_headers(data)
