b3ba7c32f70b2383a57270caee0c17da2bd448c2
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-03-16 11:29:02 +0100

    Use HTTPResponse in server handle

--- backend/src/server/http_server.py
+++ backend/src/server/http_server.py
@@ -1,23 +1,14 @@
-from datetime import datetime, timezone
 from pathlib import Path
 from socket import socket
 
+from response.http_response import HTTPResponse
+from response.schema import HTTPResponseStatus
 from server.exceptions import InvalidDecoding, InvalidRequest, InvalidBodyLength
 from request.http_request import HTTPRequest
 from server.tcp_server import TCPServer
 
 
 class HTTPServer(TCPServer):
-    HEADERS = {
-        "Server": "Afonso's Server",
-        "Content-Type": "text/html",
-    }
-
-    STATUS_CODES = {
-        200: "OK",
-        404: "Not Found",
-    }
-
     TEMPLATES_PATH = Path(__file__).parent.parent / "templates"
 
     def handle_request(self, client_connection: socket) -> str:
@@ -57,36 +48,24 @@ class HTTPServer(TCPServer):
             body = body.decode(encoding="UTF-8", errors="strict") if body else None
         except UnicodeDecodeError:
             raise InvalidDecoding()
+        else:
+            request.parse_body(body)
 
-        request.parse_body(body)
+        response = HTTPResponse(http_protocol=request.protocol)
+        if request.method != "GET":
+            return response.set_status_code(HTTPResponseStatus.HTTP_501_NOT_IMPLEMENTED)
 
-        # TODO: need to parse request to check URL, get method and route action.
         # how can I efficiently route a request based on the method? would a decorator help here?
-        status_line = self.get_status_line(status_code=200)
-        response_headers = self.get_response_headers()
+        response.set_status_code(status_code=HTTPResponseStatus.HTTP_200_OK)
 
         get_request_template = self.TEMPLATES_PATH / "get_request.html"
         with get_request_template.open(mode="r", encoding="utf-8") as template:
-            response_body = template.read().format(server_name=self.HEADERS["Server"])
+            response.set_body(template.read().format(server_name=response.headers["Server"]))
 
-        response = f"{status_line}{response_headers}\r\n{response_body}"
-        return response
+        status_line = response.get_status_line()
+        response.set_headers()
+        response_headers = response.get_headers()
+        response_string = f"{status_line}\r\n{response_headers}\r\n\r\n{response.body}"
 
-    def get_status_line(self, status_code: int) -> str:
-        if status_code not in self.STATUS_CODES:
-            raise Exception(f"invalid status code: '{status_code}'")
-
-        status_line = f"HTTP/1.1 {status_code} {self.STATUS_CODES[status_code]}\r\n"
-        return status_line
-
-    def get_response_headers(self, extra_headers: dict[str, str] = None) -> str:
-        headers = self.HEADERS.copy()
-        headers["Date"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S+00:00")
-
-        if extra_headers:
-            headers.update(extra_headers)
-
-        response_headers = "".join(
-            f"{header_name}: {header_value}\r\n" for header_name, header_value in headers.items()
-        )
-        return response_headers
+        # I feel like this should return an HTTPResponse, however, socket.send requires a string
+        return response_string
