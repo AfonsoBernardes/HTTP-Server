f3ccc66b88c1c053910d8df122d06e621ed26422
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-03-29 11:44:07 +0200

    Free function to parse request headers

--- backend/src/request/http_request.py
+++ backend/src/request/http_request.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
 from request.exceptions import (
     InvalidHTTPHeaders,
@@ -9,43 +9,49 @@ from request.schema import HTTPRequestMethod
 from server.schema import HTTPProtocol
 
 
+def parse_headers(request_headers: str) -> Tuple[HTTPRequestMethod, str, Dict[str, str], ]:
+    request_headers = request_headers.splitlines()
+
+    # parse request line: <METHOD> <TARGET> <PROTOCOL>
+    request_line = request_headers.pop(0)
+    request_line = request_line.split(" ")
+
+    headers = {}
+    for header in request_headers:
+        try:
+            key, value = header.split(": ", maxsplit=1)
+            headers[key] = value
+        except ValueError:
+            raise InvalidHTTPHeaders()
+
+    try:
+        method = HTTPRequestMethod(request_line[0])
+    except ValueError:
+        raise InvalidHTTPMethod(request_line[0])
+
+    url = request_line[1]
+
+    # validate protocol
+    protocol = request_line[2]
+    try:
+        HTTPProtocol(protocol)
+    except ValueError:
+        raise InvalidHTTPProtocol(request_line[2])
+
+    return method, url, headers
+
+
 class HTTPRequest:
     method: HTTPRequestMethod
     url: str
-    protocol: HTTPProtocol
     headers: Dict[str, str]
     body: Optional[str]
 
     def __init__(self, method, url, headers):
-        self.headers: Dict[str, str] = {}
+        self.method = method
+        self.url = url
+        self.headers = headers
         self.body = None
-
-    # TODO: free function return headers, url, body and methods, verify protocol
-    def parse_headers(self, request_headers: str):
-        request_headers = request_headers.splitlines()
-
-        # parse request line: <METHOD> <TARGET> <PROTOCOL>
-        request_line = request_headers.pop(0)
-        request_line = request_line.split(" ")
-
-        for header in request_headers:
-            try:
-                key, value = header.split(": ", maxsplit=1)
-                self.headers[key] = value
-            except ValueError:
-                raise InvalidHTTPHeaders()
-
-        try:
-            self.method = HTTPRequestMethod(request_line[0])
-        except ValueError:
-            raise InvalidHTTPMethod(request_line[0])
-
-        self.url = request_line[1]
-
-        try:
-            self.protocol = HTTPProtocol(request_line[2])
-        except ValueError:
-            raise InvalidHTTPProtocol(request_line[2])
 
     # TODO: think about writing body to a file and process from there
     def parse_body(self, request_body: str):
