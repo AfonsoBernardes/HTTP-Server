06268959b7af1f70b9074718d68320e7a79a1e4f
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-05-14 17:03:52 +0200

    handlers for all HTTP methods allowed in router

--- backend/src/router/http_router.py
+++ backend/src/router/http_router.py
@@ -1,23 +1,46 @@
 from typing import Callable, Dict, Optional
 
 from request.schema import HTTPRequestMethod
+from router.exceptions import DuplicateRoute
 
 
 class HTTPRouter:
 
     def __init__(self, prefix: Optional[str] = None):
         self.routes = Dict[str, Dict[HTTPRequestMethod, Callable]] = {}
-        self.prefix = prefix
 
-    def include_route(self, path: str, method: HTTPRequestMethod, handler: Callable) -> None:
-        self.routes[path] = {method: handler}
+    def include_route(self, route: str, method: HTTPRequestMethod, handler: Callable) -> None:
+        if not self.routes.get(route):
+            self.routes[route] = {}
+
+        if method in self.routes[route]:
+            raise DuplicateRoute(route, method)
+
+        self.routes[route][method] = handler
+
 
     # Helper handlers
-    def get(self):
-        pass
+    def get(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.GET)
 
-    def _decorator(self, path: str, method: HTTPRequestMethod):
+    def delete(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.DELETE)
+
+    def patch(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.PATCH)
+
+    def post(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.POST)
+
+    def put(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.PUT)
+
+    def head(self, route: str) -> Callable:
+        return self._decorator(route, HTTPRequestMethod.HEAD)
+
+    def _decorator(self, route: str, method: HTTPRequestMethod) -> Callable:
         def _wrapper(handler: Callable) -> Callable:
-            pass
+            self.include_route(route, method, handler)
+            return handler
 
         return _wrapper
