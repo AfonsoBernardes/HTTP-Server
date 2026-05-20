07f972c00d03b29bb88984995af7f64269afc61f
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-05-20 17:42:56 +0200

    Add logic to retrieve users from .jsn file. Use Response as return type from endpoint

--- backend/src/api/users.py
+++ backend/src/api/users.py
@@ -1,8 +1,17 @@
+import json
+from pathlib import Path
+
+from api.schema import Response, ContentType
 from router.http_router import HTTPRouter
+
+DATABASE_PATH = Path("./src/database")
 
 router = HTTPRouter()
 
 
 @router.get("")
-def get_users():
-    print("GET USERS ROUTER")
+def get_users() -> Response:
+    users_data = DATABASE_PATH / "users.json"
+    with users_data.open("r", encoding="utf-8") as file:
+        users = json.load(file)
+        return Response(content_type=ContentType.JSON, data=users)
