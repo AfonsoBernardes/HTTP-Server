5f4eb403a233f34c73948d2b5f1d52400e89b1cc
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-05-25 12:03:43 +0200

    Add TODO

--- backend/src/server/http_server.py
+++ backend/src/server/http_server.py
@@ -17,6 +17,7 @@ class HTTPServer(TCPServer):
         self.routers: Dict[str, HTTPRouter] = {}
 
     def include_router(self, prefix: str, router: HTTPRouter) -> None:
+        # TODO: make separate None prefix to make it optinal
         if prefix in self.routers:
             raise DuplicateRouterPrefix(prefix=prefix)
 
