dcfb42808e1427cfb4b01bdf1241e4ec19037295
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-05-27 18:27:25 +0200

    New exception for invalid reponse headers

--- backend/src/response/exceptions.py
+++ backend/src/response/exceptions.py
@@ -4,3 +4,7 @@ from displayable_exceptions.displayable_exception import DisplayableException
 class InvalidHTTPStatusCode(DisplayableException):
     def __init__(self, status_code: int):
         super().__init__(f"invalid HTTP status code {status_code!r}")
+
+class InvalidResponseHeader(DisplayableException):
+    def __init__(self):
+        super().__init__(f"invalid reponse header")
