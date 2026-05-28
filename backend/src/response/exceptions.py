c4f21c104df4da94f67ffa7720ec635b176010d4
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-05-28 11:17:10 +0200

    Reformat

--- backend/src/response/exceptions.py
+++ backend/src/response/exceptions.py
@@ -5,6 +5,7 @@ class InvalidHTTPStatusCode(DisplayableException):
     def __init__(self, status_code: int):
         super().__init__(f"invalid HTTP status code {status_code!r}")
 
+
 class InvalidResponseHeader(DisplayableException):
     def __init__(self):
         super().__init__(f"invalid response header")
