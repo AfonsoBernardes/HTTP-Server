6ab8c60df8726bc2f17b79b5ae33b56f658cbbd1
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-06-04 15:56:18 +0200

    Add status codes to each exception

--- backend/src/response/exceptions.py
+++ backend/src/response/exceptions.py
@@ -2,23 +2,32 @@ from typing import Any
 
 from api.schema import ContentType
 from displayable_exceptions.http_exception import HTTPServerException
+from response.schema import HTTPResponseStatusCode
 
 
 class InvalidHTTPStatusCode(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_500
+
     def __init__(self, status_code: int):
         super().__init__(f"invalid HTTP status code {status_code!r}")
 
 
 class InvalidResponseHeader(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_500
+
     def __init__(self):
         super().__init__(f"invalid response header")
 
 
 class InvalidContent(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_500
+
     def __init__(self):
         super().__init__(f"invalid response header")
 
 
 class InvalidBody(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_500
+
     def __init__(self, body: Any, content_type: ContentType):
         super().__init__(f"body {body!r} cannot be serialized as {content_type.value!r}")
--- backend/src/router/exceptions.py
+++ backend/src/router/exceptions.py
@@ -1,27 +1,32 @@
-from displayable_exceptions.http_exception import HTTPServerException
+from displayable_exceptions.http_exception import HTTPServerException, DisplayableException
 from request.schema import HTTPRequestMethod
+from response.schema import HTTPResponseStatusCode
 
 
 class URLNotFound(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_404
+
     def __init__(self, url: str):
         super().__init__(f"URL {url!r} not found")
 
 
 class HandlerNotFound(HTTPServerException):
+    status_code = HTTPResponseStatusCode.HTTP_405
+
     def __init__(self, url: str, method: HTTPRequestMethod):
         super().__init__(f"no handler found for {method.value!r} {url!r}")
 
 
-class DuplicateRoute(HTTPServerException):
+class DuplicateRoute(DisplayableException):
     def __init__(self, path: str, method: HTTPRequestMethod):
         super().__init__(f"{method.value!r} already exists for path {path!r}")
 
 
-class DuplicateRouter(HTTPServerException):
+class DuplicateRouter(DisplayableException):
     def __init__(self):
         super().__init__("router already exists")
 
 
-class DuplicateRouterPrefix(HTTPServerException):
+class DuplicateRouterPrefix(DisplayableException):
     def __init__(self, prefix: str):
         super().__init__(f"a router with prefix {prefix!r} already exists")
