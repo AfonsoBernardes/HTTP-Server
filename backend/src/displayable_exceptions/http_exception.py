331ec0d80d2bb6b130f16145105a3904303fdbd9
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-06-04 17:51:54 +0200

    Add base_message (without uuid) for testing purposes

--- backend/src/displayable_exceptions/http_exception.py
+++ backend/src/displayable_exceptions/http_exception.py
@@ -5,6 +5,7 @@ from response.schema import HTTPResponseStatusCode
 
 class DisplayableException(Exception):
     def __init__(self, message: str):
+        self.base_message = message
         self.exception_id = uuid4()
         self.message = f"{message}: {self.exception_id}"
         super().__init__(self.message)
