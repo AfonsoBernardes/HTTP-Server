5074771311a58d68725b1bcf6880eb004a708435
AfonsoBernardes <afonso.duarte.bernardes@gmail.com>
2026-06-10 09:18:08 +0200

    Move body parsing from server to request method

--- backend/src/request/http_request.py
+++ backend/src/request/http_request.py
@@ -1,12 +1,20 @@
+from socket import socket
 from typing import Dict, List, Optional, Tuple
 
 from request.exceptions import (
+    BodyTooLarge,
     DuplicateHTTPHeader,
+    InvalidBodyLength,
+    InvalidContentLength,
     InvalidHTTPHeaders,
     InvalidHTTPMethod,
     InvalidHTTPProtocol,
+    InvalidTransferEncoding,
+    UnspecifiedBodyLength,
+    UnsupportedTransferEncoding,
 )
 from request.schema import HTTPRequestMethod
+from server.exceptions import InvalidDecoding
 from server.schema import HTTPProtocol
 
 SINGLE_VALUE_HEADERS = {
@@ -72,6 +80,8 @@ class HTTPRequest:
     headers: Dict[str, str | List[str]]
     body: Optional[str]
 
+    MAX_BODY_SIZE = 1 * 1024 * 1024
+
     def __init__(self, method, url, protocol, headers):
         self.method = method
         self.url = url
@@ -80,5 +90,62 @@ class HTTPRequest:
         self.body = None
 
     # TODO: think about writing body to a file and process from there
-    def parse_body(self, request_body: Optional[str]):
-        self.body = request_body if request_body else None
+    def parse_body(self, client_connection: socket, body: bytes) -> Optional[str]:
+        # keys are already lower case from "parse_headers" function
+        transfer_encoding = self.headers.get("transfer-encoding", None)
+        content_length = self.headers.get("content-length", None)
+
+        # TODO: Wrong Chunked Parsing:
+        #  1. Need to parse each chunk as declared on length prefix in hex
+        if transfer_encoding is not None:
+            if len(transfer_encoding) != 1:
+                raise UnsupportedTransferEncoding(transfer_encoding=transfer_encoding)
+
+            transfer_encoding = transfer_encoding[0]
+            if transfer_encoding.lower() == "chunked":
+                while True:
+                    chunk_data = client_connection.recv(1024)
+                    if len(chunk_data) == 0:
+                        break
+                    body += chunk_data
+            elif transfer_encoding.lower() in ("compress", "deflate", "gzip"):
+                raise UnsupportedTransferEncoding(transfer_encoding=transfer_encoding)
+            else:
+                raise InvalidTransferEncoding(transfer_encoding=transfer_encoding)
+
+        elif content_length is not None:  # "Content-Length" is present
+            content_length = content_length[0]
+            try:
+                content_length = int(content_length)  # "Content-Length" should be unique
+            except ValueError:  # can't conver to integer, like empty string
+                raise InvalidContentLength(content_length=content_length)
+            else:  # can convert to integer but still invalid like negative number
+                if content_length < 0:
+                    raise InvalidContentLength(content_length=content_length)
+                elif content_length > self.MAX_BODY_SIZE:
+                    raise BodyTooLarge(max_body_size=self.MAX_BODY_SIZE, content_length=content_length)
+
+            if content_length == 0:
+                body = b""
+            else:
+                while len(body) < content_length:
+                    chunk_data = client_connection.recv(1024)
+                    if not chunk_data:
+                        break
+                    body += chunk_data
+
+                if len(body) < content_length:
+                    raise InvalidBodyLength(body_length=len(body), expected_length=content_length)
+                else:
+                    body = body[:content_length] if content_length > 0 else b""
+
+        elif self.method in (HTTPRequestMethod.POST, HTTPRequestMethod.PUT, HTTPRequestMethod.PATCH):
+            raise UnspecifiedBodyLength(method=self.method)
+
+        try:
+            body = body.decode(encoding="UTF-8", errors="strict") if body else None
+        except UnicodeDecodeError:
+            raise InvalidDecoding()
+
+        self.body = body if body else None
+        return self.body
